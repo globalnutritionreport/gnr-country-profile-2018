6b8d2845709f5f31cd71007d2fe890dc1ec1d2ba
Alex Miller <alex.k.miller@gmail.com>
2018-11-14 11:34:37 -0500

    jpg for smaller thumbnails

--- thumbs.sh
+++ thumbs.sh
@@ -3,5 +3,5 @@
 for i in ./pdfs/*.pdf; do
   file=${i##*[/|\\]}
   base=${file%.*}
-  convert $i[0] -flatten ./thumbs/$base.png
+  convert $i[0] -flatten -resize 450x800 -density 300 -quality 90 ./thumbs/$base.jpg
 done;
