03e55350268d45adbbefbe5ebf4f5612bac7d023
Alex Miller <alex.k.miller@gmail.com>
2018-11-26 09:40:14 -0500

    Sections

--- collate_data.R
+++ collate_data.R
@@ -13,23 +13,61 @@ world = read.csv("data_world.csv",na.strings="",as.is=T)
 
 wb <- createWorkbook("all_profile_data.xlsx")
 
-components = unique(country$component)
-components = sort(components)
+com.dict = c(
+  "A"="overview",
+  "C"="child nutrition",
+  "G"="adolescent",
+  "H"="adult",
+  "I"="adult",
+  "K"="child feeding",
+  "M"="dietary needs",
+  "O"="policy",
+  "P"="intervention",
+  "Q"="policy",
+  "R"="economics",
+  "S"="underlying",
+  "U"="underlying",
+  "V"="underlying"
+)
 
-for(this.component in components){
-  ctry.sub = subset(country,component==this.component)
-  reg.sub = subset(region,component==this.component)
-  wld.sub = subset(world,component==this.component)
+com.dict.override = c(
+  "coexistence"="child nutrition",
+  "ODA_specific"="ODA",
+  "ODA_received"="ODA"
+)
+
+country$section = NA
+country$section = com.dict[country$component]
+country$section[which(country$indicator %in% names(com.dict.override))] = com.dict.override[country$indicator[which(country$indicator %in% names(com.dict.override))]]
+country$component = NULL
+
+region$section = NA
+region$section = com.dict[region$component]
+region$section[which(region$indicator %in% names(com.dict.override))] = com.dict.override[region$indicator[which(region$indicator %in% names(com.dict.override))]]
+region$component = NULL
+
+world$section = NA
+world$section = com.dict[world$component]
+world$section[which(world$indicator %in% names(com.dict.override))] = com.dict.override[world$indicator[which(world$indicator %in% names(com.dict.override))]]
+world$component = NULL
+
+sections = unique(country$section)
+sections = sort(sections)
+
+for(this.section in sections){
+  ctry.sub = subset(country,section==this.section)
+  reg.sub = subset(region,section==this.section)
+  wld.sub = subset(world,section==this.section)
   
-  ctry.sheet = paste("Country",this.component,sep=".")
+  ctry.sheet = paste("Country",this.section)
   addWorksheet(wb,ctry.sheet)
   writeData(wb,sheet=ctry.sheet,ctry.sub,colNames=TRUE,rowNames=FALSE)
   
-  reg.sheet = paste("Region",this.component,sep=".")
+  reg.sheet = paste("Region",this.section)
   addWorksheet(wb,reg.sheet)
   writeData(wb,sheet=reg.sheet,reg.sub,colNames=TRUE,rowNames=FALSE)
   
-  wld.sheet = paste("World",this.component,sep=".")
+  wld.sheet = paste("World",this.section)
   addWorksheet(wb,wld.sheet)
   writeData(wb,sheet=wld.sheet,wld.sub,colNames=TRUE,rowNames=FALSE)
 }
